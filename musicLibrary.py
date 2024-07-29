b3eeab34859bd180943ecab1852d57f484703e66
Ankush_Kapoor <121434345+Ankush1626@users.noreply.github.com>
2024-07-29 17:46:44 +0530

    Initial Commit

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music={
+    "skyfall": "https://youtu.be/DeumyOzKqgI?si=fYTKMCCo2PEPQmtv",
+    "bye-bye": "https://youtu.be/Eo-KmOd3i7s?t=60"
+}
+
+
+#You can add more song name and links to this file
